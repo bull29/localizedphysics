f5608887556b95594a58c850ca3cf01ed33dc2f7
DavidRalphsky <garrysmodrox@yahoo.com>
2016-03-18 02:20:25 -0300

    Constraints update
    Thanks Bull

--- lua/gravityhull/sv_main.lua
+++ lua/gravityhull/sv_main.lua
@@ -190,7 +190,7 @@ end
 -- Name: UpdateHull
 -- Desc: Create or update a gravity hull's ghost, including moving parts.
 ------------------------------------------------------------------------------------------
-function GH.UpdateHull(ent,gravnormal)
+function GH.UpdateHull(ent,gravnormal,includeConstraints)
 	if !(IsValid(ent) and GH.SHIPS[ent]) then return end
 	local xcon = GH.ConstrainedEntities(ent) --this is just for the update check
 	local gents = GH.SHIPS[ent].Ghosts
@@ -203,7 +203,7 @@ function GH.UpdateHull(ent,gravnormal)
 	--Adds any prop connected solidly to ent as part of its hull,
 	--and any prop connected with a nonsolid constraint to the parts list.
 	--Also adds other constrained hulls as special parts.
-	if ent.Constraints then
+	if ent.Constraints && includeConstraints == 1 then -- Thanks Bull
 		while #tbtab > 0 do
 			local bd = tbtab[#tbtab]
 			local bde = bd[1]
