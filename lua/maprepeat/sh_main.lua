15f68bc314441af9cb8a48f2fc7ed29828512e82
DavidRalphsky <garrysmodrox@yahoo.com>
2016-03-25 16:31:10 -0300

    Admin cleanup stack overflow hotfix
    Thanks MiniGui98!

--- lua/maprepeat/sh_main.lua
+++ lua/maprepeat/sh_main.lua
@@ -188,7 +188,10 @@ if !RealCleanUpMap then
 	RealCleanUpMap = game.CleanUpMap
 end
 function game.CleanUpMap(send, filters)
-	if !MapRepeat then return game.CleanUpMap(send,filters)end
+	if !MapRepeat then 
+		game.CleanUpMap = RealCleanUpMap
+		return game.CleanUpMap(send,filters)
+	end
 	if type(filters) != 'table' then filters = {filters} end
 	if !table.HasValue(filters,"func_brush") then filters[#filters+1] = "func_brush" end
 	return RealCleanUpMap(send, filters)
