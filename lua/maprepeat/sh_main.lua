c941e3475753859f69aad5182df0e1c6c144c915
David Ralphsky <davidralphsky@yahoo.com>
2016-11-04 12:59:10 -0400

    Updated Comments

--- lua/maprepeat/sh_main.lua
+++ lua/maprepeat/sh_main.lua
@@ -5,24 +5,24 @@ function MapRepeat.CellToArray(cell) -- Get values from the cell
 	if !cell then return end
 	local c = {}
 	local i = string.find(cell,' ') -- Find the spaces in between the numbers we want
-	c[1] = string.sub(cell,1,i-1) -- Kind of hacky way to get the X
- 	c[2] = string.sub(cell,i+1,string.find(cell,' ',i+1)-1) -- Getting pretty hacky to find Y
-	c[3] = string.sub(cell,string.find(cell,' ',i+1)+1) -- Hackasaurus Rex to get Z
+	c[1] = string.sub(cell,1,i-1) -- Find X
+ 	c[2] = string.sub(cell,i+1,string.find(cell,' ',i+1)-1) -- Find Y
+	c[3] = string.sub(cell,string.find(cell,' ',i+1)+1) -- Find Z
 	return c
 end
-local srv_genned = {}
+local srv_genned = {} -- Make an empty table
 function MapRepeat.GenCell(cell) -- Generate the cell!
-	if !cell then return end
+	if !cell then return end -- If invalid cell, abort
 	local c = MapRepeat.CellToArray(cell) -- Get the values from the cell
 	MapRepeat.Cells[cell] = MapRepeat.Cells[cell] or {} -- Script error prevention
-	if MapRepeat.Cells[cell].gen then return end -- If the cell is labelled as generated, ignore it
-	MapRepeat.Cells[cell].gen = true -- Label the cell as generated (stop the cell from being genned again)
-	for e,t in pairs(MapRepeat.RGen) do -- For all of the entities, run RGen
+	if MapRepeat.Cells[cell].gen then return end -- If the cell has already been generated, abort
+	MapRepeat.Cells[cell].gen = true -- Stop the cell from being generated again (related to the above line^)
+	for e,t in pairs(MapRepeat.RGen) do -- If we need to randomly generate
 		for k,v in pairs(t) do
 			if type(v) == 'table' then -- If the value is a table (which we want)
-				local pass = true -- Random variable that only superllama knows about #1
-				local send = false -- Random variable that only superllama knows about #2
-				local p = {false,false,false} -- Random table that only superllama knows about
+				local pass = true -- First pass
+				local send = false -- Default send to false
+				local p = {false,false,false} -- X Y and Z haven't been set up yet.
 				for i=1,3 do -- For loop to get the three values, X Y and Z
 					local token = string.sub(v[i],1,1) -- Find X Y and Z
 					if token == '%' and CLIENT then -- If it's a random chance, run the following on client
@@ -30,24 +30,24 @@ function MapRepeat.GenCell(cell) -- Generate the cell!
 							RunConsoleCommand("sl_mr_gencell",cell) -- Generate it
 							srv_genned[cell] = true -- Label it as generated
 						end
-						pass = false -- Set random variable that only superllama knows about #1 to false
+						pass = false -- Reset pass
 					elseif token == '%' and SERVER then -- If it's a random chance, run on server:
 						pass = (math.random(1,100) < (tonumber(string.sub(v[i],2)) or 0)) -- Do random chance
-						send = true -- Set random variable that only superllama knows about #2 to true
-						p[i] = true -- Set random table that only superllama knows about to true
+						send = true -- Set up send
+						p[i] = true -- Value has been set (X Y or Z)
 					end 
 				end
-				if	pass and -- If random variable that only superllama knows about #1 is true/more than 0
-				    ((v[1] == c[1]) or (v[1] == '?') or p[1]) and -- If X is a number, ?, or percent
-					((v[2] == c[2]) or (v[2] == '?') or p[2]) and -- If Y is a number, ?, or percent
-					((v[3] == c[3]) or (v[3] == '?') or p[3]) then -- If Z is a number, ?, or percent
-					if send then -- If random variable that only superllama knows about is set to true/more than 0
+				if pass and -- If we've done the first pass
+				    ((v[1] == c[1]) or (v[1] == '?') or p[1]) and -- If X is a number, ?, or has been RGen'd
+					((v[2] == c[2]) or (v[2] == '?') or p[2]) and -- If Y is a number, ?, or has been RGen'd
+					((v[3] == c[3]) or (v[3] == '?') or p[3]) then -- If Z is a number, ?, or has been RGen'd
+					if send then -- If we can send
 						MapRepeat.AddCell(e,cell) -- Add the cell
 					else -- If not,
 						MapRepeat.Cells[cell][e] = true -- Set ent to be in the cell
-						if SERVER and e and e != NULL then 
+						if SERVER and e and e != NULL then -- If e isn't invalid, run serverside:
 							e.Cells = e.Cells or {} -- Script error prevention
-							e.Cells[#e.Cells+1] = cell -- Go to next index.
+							e.Cells[#e.Cells+1] = cell -- Add to cell table
 						end
 					end
 				end
@@ -60,31 +60,31 @@ function MapRepeat.InCell(e,cell) --  Is this ent in this cell?
 	if SERVER then 
 		if !e.Cells then return false end -- If the ent doesn't have a cell, say no
 		for _,c in pairs(e.Cells) do
-			if c == cell then return true end -- If the ent's cell matches the other argument, say yes
+			if c == cell then return true end -- If the ent's cell matches the cell table, say yes
 		end
-		return false -- But...also say no(???)
+		return false -- Otherwise, say no
 	else return (MapRepeat.Cells[cell] and (MapRepeat.Cells[cell][e] or MapRepeat.Cells[cell][e:EntIndex()])) or MapRepeat.CelledEnts[e] == cell or MapRepeat.CelledEnts[e:EntIndex()] == cell end
-	-- ^Really hacky stuff don't worry about it
+	-- ^Crazy stuff to find out the cell
 end
-function MapRepeat.CellToPos(_pos,cell) -- Get what cell the position is in
+function MapRepeat.CellToPos(_pos,cell) -- Get position in cell
 	local pos = _pos
-	local c = MapRepeat.CellToArray(cell)
-	if !c then return pos end
+	local c = MapRepeat.CellToArray(cell) -- Get cell values
+	if !c then return pos end -- If the cell has no values, just return pos
 	local s = MapRepeat.Sync
-	local cx = (s.right or 0) - (s.left or 0)
-	local cy = (s.bottom or 0) - (s.top or 0)
-	local cz = (s.up or 0) - (s.down or 0)
+	local cx = (s.right or 0) - (s.left or 0) -- Right/Left
+	local cy = (s.bottom or 0) - (s.top or 0) -- Bottom/Top
+	local cz = (s.up or 0) - (s.down or 0) -- Up/Down
 	pos.x = pos.x + (cx * (tonumber(c[1]) or 0))
 	pos.y = pos.y + (cy * (tonumber(c[2]) or 0))
 	pos.z = pos.z + (cz * (tonumber(c[3]) or 0))
 	return pos
 end
-function MapRepeat.PosToCell(_pos,_pos2) -- Get the position relative to the cell
+function MapRepeat.PosToCell(_pos,_pos2) -- Get the cell the position is in
 	local pos,pos2 = _pos,_pos2
 	local s = MapRepeat.Sync
-	local cx = (s.right or 0) - (s.left or 0)
-	local cy = (s.bottom or 0) - (s.top or 0)
-	local cz = (s.up or 0) - (s.down or 0)
+	local cx = (s.right or 0) - (s.left or 0) -- Right/Left
+	local cy = (s.bottom or 0) - (s.top or 0) -- Bottom/Top
+	local cz = (s.up or 0) - (s.down or 0) -- Up/Down
 	local x,y,z
 	if pos.x >= 0 then x = math.floor((pos.x+(s.right or 0))/cx)
 	else x = math.ceil((pos.x+(s.left or 0))/cx) end
@@ -103,74 +103,74 @@ function MapRepeat.PosToCell(_pos,_pos2) -- Get the position relative to the cel
 	if x == -0 then x = 0 end
 	if y == -0 then y = 0 end
 	if z == -0 then z = 0 end
-	return x..' '..y..' '..z, pos,pos2
+	return x..' '..y..' '..z, pos,pos2 -- Return cell number as a string and the two positions
 end
-if !util.RealTraceLine then -- If the function is broken or disabled, use the usual one
-	util.RealTraceLine = util.TraceLine
+if !util.RealTraceLine then -- If we don't know what RealTraceLine is
+	util.RealTraceLine = util.TraceLine -- RealTraceLine is just a duplicate of TraceLine
 end
 function util.TraceLine(_tr) -- Override for TraceLine
-	if !MapRepeat then
-		util.TraceLine = util.RealTraceLine
-		return util.TraceLine(_tr)
+	if !MapRepeat then -- If MapRepeat isn't active
+		util.TraceLine = util.RealTraceLine -- Reset to normal TraceLine
+		return util.TraceLine(_tr) -- Perform normal TraceLine
 	end
 	local tr,cell = _tr,nil
 	local s = MapRepeat.Sync
-	cell,tr.start,tr.endpos = MapRepeat.PosToCell(tr.start,tr.endpos)
-	for _,e in pairs(ents.GetAll()) do
-		if !MapRepeat.InCell(e,cell) && (CLIENT or e:GetMoveType() != MOVETYPE_NONE) then
-			if type(tr.filter) != 'table' then tr.filter = {tr.filter} end
-			tr.filter[#tr.filter+1] = e
+	cell,tr.start,tr.endpos = MapRepeat.PosToCell(tr.start,tr.endpos) -- Get cell the TraceLine is in
+	for _,e in pairs(ents.GetAll()) do -- Get all entities
+		if !MapRepeat.InCell(e,cell) && (CLIENT or e:GetMoveType() != MOVETYPE_NONE) then -- If the entity isn't in your cell
+			if type(tr.filter) != 'table' then tr.filter = {tr.filter} end 
+			tr.filter[#tr.filter+1] = e -- Add the entity to the filters list (so it won't hit stuff not in its cell)
 		end
 	end
-	local tro = util.RealTraceLine(tr)
+	local tro = util.RealTraceLine(tr) -- Make a new trace so we don't stack overflow
 	tro.HitPos = MapRepeat.CellToPos(tro.HitPos,cell)
 	tro.StartPos = tr.start
-	return tro
+	return tro -- Return the TraceLine with the new filters
 end
 
 --
 -- WE NEED MORE UTIL TRACES SIR, WE NEED MORE.
 --
 
-if !util.RealTraceEntity then
-	util.RealTraceEntity = util.TraceEntity
+if !util.RealTraceEntity then -- If we don't know what RealTraceEntity
+	util.RealTraceEntity = util.TraceEntity -- RealTraceEntity is just a duplicate of TraceEntity
 end
-function util.TraceEntity(te,ent)
-	if !MapRepeat then
-		util.TraceEntity = util.RealTraceEntity
-		return util.TraceEntity(te,ent)
+function util.TraceEntity(te,ent) -- Override  for TraceEntity
+	if !MapRepeat then -- If MapRepeat isn't active
+		util.TraceEntity = util.RealTraceEntity -- Reset to normal TraceEntity
+		return util.TraceEntity(te,ent) -- Perform normal TraceEntity
 	end
 	
-	cell,te.start,te.endpos = MapRepeat.PosToCell(te.start,te.endpos)
-	for _,e in pairs(ents.GetAll()) do
-		if !MapRepeat.InCell(e,cell) then
+	cell,te.start,te.endpos = MapRepeat.PosToCell(te.start,te.endpos) -- Get the cell the TraceEntity is in
+	for _,e in pairs(ents.GetAll()) do -- Get all entities
+		if !MapRepeat.InCell(e,cell) then -- If the entity isnt in your cell
 			if type(te.filter) != 'table' then te.filter = {te.filter} end
-				te.filter[#te.filter+1] = e
+				te.filter[#te.filter+1] = e -- Add the entities to the filters list
 		end
 	end
 	
-	local teo = util.RealTraceEntity(te,ent)
+	local teo = util.RealTraceEntity(te,ent) -- Make a new trace so we don't stack overflow
 	teo.HitPos = MapRepeat.CellToPos(teo.HitPos,cell)
 	teo.StartPos = te.start
-	return teo
+	return teo -- Return TraceEntity with the new filters
 end
 
-if !util.RealTraceHull then
-	util.RealTraceHull = util.TraceHull
+if !util.RealTraceHull then -- If we don't know what RealTraceHull is
+	util.RealTraceHull = util.TraceHull -- RealTraceHull is just a duplicate of TraceHull
 end
-function util.TraceHull(th)
-	if !MapRepeat then
-		util.TraceHull = util.RealTraceHull
-		return util.TraceHull(th)
+function util.TraceHull(th) -- Override for TraceHull
+	if !MapRepeat then -- If MapRepeat isn't active
+		util.TraceHull = util.RealTraceHull -- Reset to normal TraceHull
+		return util.TraceHull(th) -- Perform normal TraceHull
 	end
 
-	cell,th.start,th.endpos = MapRepeat.PosToCell(th.start,th.endpos)
+	cell,th.start,th.endpos = MapRepeat.PosToCell(th.start,th.endpos) -- Get the cell the TraceHull is in
 	
-	local tho = util.RealTraceHull( {
+	local tho = util.RealTraceHull( { -- Make a new trace so we don't stack overflow
 	start = th.start,
 	endpos = th.endpos,
 	filter = function(e)
-		if(type(th.filter) == 'function') then
+		if(type(th.filter) == 'function') then -- Function to add entities to new filter
 			pass = th.filter(e)
 		else
 			pass = table.HasValue(th.filter,e)
@@ -181,41 +181,41 @@ function util.TraceHull(th)
 	maxs = th.maxs,
 	} )
 	
-	return tho
+	return tho -- Return TraceHull with new filters
 end
 
-if !util.RealQuickTrace then
-	util.RealQuickTrace = util.QuickTrace
+if !util.RealQuickTrace then -- If we don't know what RealQuickTrace is
+	util.RealQuickTrace = util.QuickTrace -- RealQuickTrace is just a duplicate of QuickTrace
 end
-function util.QuickTrace(origin,dir,filter)
-	if !MapRepeat then
-		util.QuickTrace = util.RealQuickTrace
-		return util.QuickTrace(origin,dir,filter)
+function util.QuickTrace(origin,dir,filter) -- Override for QuickTrace
+	if !MapRepeat then -- If MapRepeat isn't active
+		util.QuickTrace = util.RealQuickTrace -- Reset to normal QuickTrace
+		return util.QuickTrace(origin,dir,filter) -- Perform normal QuickTrace
 	end
 
-	cell = MapRepeat.PosToCell(origin,dir*2147483647)
-	local nfilter = {}
+	cell = MapRepeat.PosToCell(origin,dir) -- Find cell the QuickTrace is in 
+	local nfilter = {} -- Make a new filter
 	
-	for _,e in pairs(ents.GetAll()) do
-		if !MapRepeat.InCell(e,cell) then
-			nfilter[#nfilter+1] = e
+	for _,e in pairs(ents.GetAll()) do -- Get all entities
+		if !MapRepeat.InCell(e,cell) then -- If the entity isn't in your cell
+			nfilter[#nfilter+1] = e -- Add it to the filters
 		end
 	end	
 
-	local qto = util.RealQuickTrace(origin,dir,nfilter)
+	local qto = util.RealQuickTrace(origin,dir,nfilter) -- Make a new trace so we don't stack overflow
 	
-	return qto
+	return qto -- Return QuickTrace with new filters
 end
 
-if !RealCleanUpMap then
-	RealCleanUpMap = game.CleanUpMap
+if !RealCleanUpMap then -- If we don't know what RealCleanUpMap is
+	RealCleanUpMap = game.CleanUpMap -- RealCleanUpMap is just a duplicate of CleanUpMap
 end
-function game.CleanUpMap(send, filters)
-	if !MapRepeat then 
-		game.CleanUpMap = RealCleanUpMap
-		return game.CleanUpMap(send,filters)
+function game.CleanUpMap(send, filters) -- Override for CleanUpMap
+	if !MapRepeat then -- If MapRepeat isn't active
+		game.CleanUpMap = RealCleanUpMap -- Reset to normal CleanUpMap
+		return game.CleanUpMap(send,filters) -- Perform normal CleanUpMap
 	end
-	if type(filters) != 'table' then filters = {filters} end
-	if !table.HasValue(filters,"func_brush") then filters[#filters+1] = "func_brush" end
-	return RealCleanUpMap(send, filters)
+	if type(filters) != 'table' then filters = {filters} end -- If the filters aren't a table, set them to a table
+	if !table.HasValue(filters,"func_brush") then filters[#filters+1] = "func_brush" end -- Add all func_brushes to the filters
+	return RealCleanUpMap(send, filters) -- Return CleanUpMap with the new filters
 end
